92bb152d5f6240696b2aca7aa31196cde2440c37
Yash sharma <yashsharmaofficially@gmail.com>
2025-12-27 13:53:06 +0530

    changed port

--- backend/index.js
+++ backend/index.js
@@ -12,4 +12,8 @@ app.use("/api/v1",mainRouter)
 app.get("/", (req, res) => {
   res.send("Server is alive");
 });
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
