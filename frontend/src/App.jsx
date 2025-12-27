d0031e648b11c4500b9184b6b3a728b23566b63e
Yash sharma <yashsharmaofficially@gmail.com>
2025-12-27 13:47:01 +0530

    Add start script

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,13 @@ import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin"
 import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
+import { Landing } from "./pages/Landing";
 function App(){
   return (
     <div>
       <BrowserRouter>
       <Routes>
+        <Route path = "/" element={<Landing />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path = "/signin" element={< Signin />}></Route>
          <Route path="/dashboard" element={<Dashboard />} />
