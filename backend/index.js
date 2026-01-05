e087d98efb86b2ba14055e247293b2d686e9eada
Yash sharma <yashsharmaofficially@gmail.com>
2026-01-05 15:13:54 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,5 @@ app.use("/api/v1",mainRouter)
 app.get("/", (req, res) => {
   res.send("Server is alive");
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
 
